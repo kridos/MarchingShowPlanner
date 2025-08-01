38927afb3abd26f3248840e9e2ec40d7d6750dc9
Krish Doshi <krishpdoshi@gmail.com>
2025-08-01 02:35:40 -0700

    adding static view
    Added changing color for circles, updating player_id, created a side menu for the pyplot, and made a static pyplot

--- animation.py
+++ animation.py
@@ -3,16 +3,20 @@ from matplotlib import animation
 import numpy as np
 
 interval_amt = 50
+fig = plt.gcf()
+ax = plt.axes(xlim = (0, 120), ylim = (0, 53.33333))
 
 def createCircle(position):
     circle = plt.Circle(
         position,        # Center at (x=5, y=5)
-        radius=0.5,     # Radius of 2.0 units
+        radius=1,     # Radius of 2.0 units
         color='red',     # Fill color
         alpha=1.0,       # Make fill transparent
         ec='black',    # Edge color
         lw=1             # Line width of the edge
     )
+    
+    ax.add_patch(circle)
     
     return circle
 
@@ -32,24 +36,23 @@ def finalUpdatePosition(players):
         
 
 
-def createAnimation(duration, players):
-    fig = plt.gcf()
-    ax = plt.axes(xlim = (0, 120), ylim = (0, 53.33333))
+def createAnimation(duration, players, static):
+    
     ax.set_aspect('equal')
     ax.set_xticks(np.arange(0, 120 + 1, 5))
     ax.set_yticks(np.arange(0, 54, 5))
     ax.grid(True, linestyle='--', alpha=0.6)
     
-    for player in players:
-        ax.add_patch(player.circle)
     
+        
     
-    anime = animation.FuncAnimation(
-        fig, updatePosition,
-        fargs=(players,),
-        frames = (duration * 1000 // interval_amt) + 1,
-        interval = interval_amt,              
-        repeat=False                          
-    )
+    if not static:
+        anime = animation.FuncAnimation(
+            fig, updatePosition,
+            fargs=(players,),
+            frames = (duration * 1000 // interval_amt) + 1,
+            interval = interval_amt,              
+            repeat=False                          
+        )
     
     plt.show()
--- main.py
+++ main.py
@@ -3,6 +3,63 @@ from functions import returnLinearFunction
 from animation import createCircle, createAnimation, finalUpdatePosition
 import tkinter as tk
 from tkinter import ttk
+import matplotlib.colors as mcolors
+
+def rgba_to_named(rgba):
+    for name, hex_val in mcolors.CSS4_COLORS.items():
+        if mcolors.to_rgba(hex_val) == tuple(rgba):
+            return name
+    return None  # No exact name match
+
+class DraggableCircle:
+    dragging_circle = None
+    
+    def __init__(self, circle, player):
+        self.circle = circle
+        self.player = player
+        self.press = None
+        self.cid_press = circle.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.cid_release = circle.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cid_motion = circle.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        if DraggableCircle.dragging_circle is not None:
+            return
+        
+        if event.inaxes != self.circle.axes:
+            return
+        contains, _ = self.circle.contains(event)
+        if not contains:
+            return
+
+        update_player.config(state="normal")
+        player_color.config(text=rgba_to_named(self.circle.get_facecolor()))
+        player_id.config(text=self.player.id)
+        DraggableCircle.dragging_circle = self
+        x0, y0 = self.circle.center
+        self.press = (x0, y0, event.xdata, event.ydata)
+
+    def on_motion(self, event):
+        if DraggableCircle.dragging_circle != self:
+            return
+        
+        if self.press is None or event.inaxes != self.circle.axes:
+            return
+        x0, y0, xpress, ypress = self.press
+        dx = event.xdata - xpress
+        dy = event.ydata - ypress
+        self.circle.center = (x0 + dx, y0 + dy)
+        self.circle.figure.canvas.draw()
+
+    def on_release(self, event):
+        if DraggableCircle.dragging_circle != self:
+            return
+        
+        DraggableCircle.dragging_circle = None
+        
+        self.press = None
+        self.player.x, self.player.y = self.circle.center
+        self.circle.figure.canvas.draw()
 
 class Field:
     def __init__(self):
@@ -27,11 +84,12 @@ class Field:
             player.setNextPosition((x, y))
             player.determineNextPath(duration)
             
-   
+    def display_static(self):
+       createAnimation(0, self.players.values(), True)
 
 
     def run_simulation(self, duration):
-        createAnimation(duration, self.players.values())
+        createAnimation(duration, self.players.values(), False)
         finalUpdatePosition(self.players.values())
         
         for player in self.players.values():
@@ -46,15 +104,15 @@ class Player:
         self.nextX = self.currentX
         self.nextY = self.currentY
         self.id = id
-        self.circle = createCircle((self.currentX, self.currentY))
-        self.nextPath = Path((self.currentX, self.currentY), (self.nextX, self.nextY), "Straight", 1)
+        self.circle = DraggableCircle(createCircle((self.currentX, self.currentY)), self)
+        self.nextPath = []
 
     def setNextPosition(self, coords):
         self.nextX = coords[0]
         self.nextY = coords[1]
 
     def determineNextPath(self, duration):
-        self.nextPath = Path((self.currentX, self.currentY), (self.nextX, self.nextY), "Straight", duration)
+        self.nextPath.append(Path((self.currentX, self.currentY), (self.nextX, self.nextY), "Straight", duration))
 
     def startMovement(self):
         startTime = time.time()
@@ -96,16 +154,108 @@ class Path:
         return (self.functionX(currentTime), self.functionY(currentTime))
 
 
-"""currField = Field()
-currField.add_player(Player((0,0), "Player 1"))
+currField = Field()
+"""currField.add_player(Player((0,0), "Player 1"))
 currField.add_player(Player((0,10), "Player 2"))
 currField.updateNextPositions(5)
 currField.run_simulation(5)
 currField.updateNextPositions(5)"""
 
-#window
 window = tk.Tk()
-window.title("GUI")
+
+def open():
+    global players_input
+    global show_preferences
+    
+    window.withdraw()
+    
+    show_preferences = tk.Toplevel()
+    show_preferences.title("Show Preferences")
+    show_preferences.geometry("600x400")
+    
+    
+    player_amt = ttk.Label(master=show_preferences, text = "Enter Amount of Players", font= 'Arial 20')
+    player_amt.pack()
+    
+    players_input = ttk.Entry(show_preferences, width=50)
+    players_input.pack()
+    
+    done_button = ttk.Button(master=show_preferences, text="Done", command=submit)
+    done_button.pack()
+
+def submit():
+    global player_id, player_color, change_id, change_color, update_player
+    
+    
+    player_amt = players_input.get()
+    
+    show_preferences.destroy()
+    
+    for i in range(int(player_amt)):
+        currField.add_player(Player((60,30), "Player " + str(i + 1)))
+        print(i)
+        
+    new_show = tk.Toplevel()
+    new_show.title("New Show")
+    new_show.geometry("600x600")
+    
+    player_id = ttk.Label(master=new_show, text = "No Player Selected", font= 'Arial 24')
+    player_id.pack()
+    
+    change_id = ttk.Entry(new_show, width=50)
+    change_id.pack()
+    
+    player_color = ttk.Label(master=new_show, text = "No Player Selected", font= 'Arial 24')
+    player_color.pack()
+    
+    #Change to dropdown
+    change_color = ttk.Entry(new_show, width=50)
+    change_color.pack()
+    
+    update_player = ttk.Button(master=new_show, text="Update Player", command=update_player_event, state="disabled")
+    update_player.pack()
+    
+    currField.display_static()
+    
+    
+def update_player_event():
+    new_player_id = change_id.get()
+    new_player_color = change_color.get()
+    
+    if new_player_id != "":
+        player = currField.players.pop(player_id.cget("text"))
+        
+        if new_player_color != "":
+            player.circle.circle.set_facecolor(new_player_color)
+            player_color.config(text=new_player_color)
+        
+        player.id = new_player_id
+        currField.add_player(player)
+        player_id.config(text=new_player_id)
+        
+    else:
+        
+        if new_player_color != "":
+            player = currField.get_player(player_id.cget("text"))
+            player.circle.circle.set_facecolor(new_player_color)
+            player_color.config(text=new_player_color)
+            
+    change_id.delete(0, tk.END)
+    change_color.delete(0, tk.END)
+
+            
+    currField.display_static()
+
+    
+    
+        
+    
+    
+        
+
+
+#window
+window.title("Create a Marching Show")
 window.geometry('600x600')
 
 #title
@@ -114,7 +264,7 @@ title_label.pack()
 
 #buttons
 button_frame = ttk.Frame(master=window)
-new_project_bt = ttk.Button(master=button_frame, text="Start New Project")
+new_project_bt = ttk.Button(master=button_frame, text="Start New Project", command=open)
 saved_project_bt = ttk.Button(master=button_frame, text="View Saved Projects")
 
 new_project_bt.pack(side = 'left', padx = 10)
@@ -123,6 +273,7 @@ button_frame.pack(pady = 10)
 
 
 window.mainloop()
+
 
 
 
